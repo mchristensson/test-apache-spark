ab5ce028721e65561acd4e43a3c7617884324f3a
MChristensson <magnus.christensson@hotmail.se>
2018-12-13 23:04:33 +0100

    Transform function defined

--- core/src/test/java/org/mac/spark/ETLContextTest.java
+++ core/src/test/java/org/mac/spark/ETLContextTest.java
@@ -1,19 +1,32 @@
 package org.mac.spark;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import org.mac.etl.ETLContext;
+import org.mac.etl.ETLMapping;
+import org.mac.etl.Segment;
 
 public class ETLContextTest {
-
+	
 	@Test
-	public void testAddSegment() {
-		fail("Not yet implemented");
+	public void testCreate() {
+		@SuppressWarnings("unused")
+		ETLContext ctx = new ETLContext();
+	}
+	
+	@Test
+	public void testAddMap() {
+		ETLContext ctx = new ETLContext();
+		ETLMapping<?, ?> mapping = ctx.addMap(null, null, null, null);
+		assertNotNull(mapping);
 	}
 
 	@Test
-	public void testAddMap() {
-		fail("Not yet implemented");
+	public void testAddSegment() {
+		ETLContext ctx = new ETLContext();
+		Segment segment = ctx.addSegment();
+		assertNotNull(segment);
 	}
 
 }
--- core/src/test/java/org/mac/spark/ETLCoreTest.java
+++ core/src/test/java/org/mac/spark/ETLCoreTest.java
@@ -1,14 +1,25 @@
 package org.mac.spark;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import org.mac.etl.ETLContext;
 import org.mac.etl.ETLCore;
 import org.mac.etl.ETLMapping;
 import org.mac.etl.Segment;
+import org.mac.etl.demo.model.Account;
+import org.mac.etl.demo.model.LegacyAccount;
+import org.mac.etl.function.ParseLong;
 
 public class ETLCoreTest {
-
+	
+	@Test
+	public void testCreate() {
+		@SuppressWarnings("unused")
+		ETLCore core = new ETLCore();
+	}
+	
 	@Test
 	public void testAddMap() {
 		ETLCore core = new ETLCore();
@@ -21,6 +32,28 @@ public class ETLCoreTest {
 		ETLCore core = new ETLCore();
 		Segment segment = core.addSegment();
 		assertNotNull(segment);
+	}
+	
+	@Test
+	public void testETLMapping() {
+
+		//Prepare mocked data
+		LegacyAccount legacyAccount = new LegacyAccount();
+		legacyAccount.setAccountNumber("123");
+		Account account = new Account();
+
+		//Setup ctx
+		ETLContext ctx = new ETLContext();
+		Segment segment = ctx.addSegment();
+
+		// Define recipe
+		ETLMapping<String, Long> map = ctx.addMap(segment, legacyAccount::getAccountNumber, ParseLong.getInstance(),
+				account::setAccountNumber);
+		
+		// Run playbook
+		map.applyTransform(legacyAccount, account);
+
+		assertEquals((long) account.getAccountNumber(), 123L);
 	}
 
 }
